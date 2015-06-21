c51efd81ab6be6726ff07bb02097c1ccda7db3c2
Izumi-Kazan <hoge_no1@hotmail.com>
2015-06-22 01:33:52 +0900

    Explosionの演出を追加

---
+++ Assets/Scrilpt/Explode.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+    /// <summary>
+    /// http://www.atmarkit.co.jp/ait/articles/1501/15/news059_3.html
+    /// </summary>
+
+    void OnMouseDown()
+    {
+        this.gameObject.GetComponent<Detonator>().Explode();
+    }
+}
